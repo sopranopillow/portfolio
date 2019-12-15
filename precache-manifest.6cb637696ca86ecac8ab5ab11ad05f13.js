12ef835a734e75eb59fcbbcc5d2259f1a607980d
Esteban Munoz <esteban.230@hotmail.com>
2019-12-14 18:31:08 -0700

    Updates

---
+++ precache-manifest.6cb637696ca86ecac8ab5ab11ad05f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c209ec84734dff606b46250653b4a46",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95a1aa008f13f377eacd",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9d1305842bbf8690fe0",
+    "url": "/portfolio/static/js/2.ac7389bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.ac7389bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95a1aa008f13f377eacd",
+    "url": "/portfolio/static/js/main.e92941e8.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
