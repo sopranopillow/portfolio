70bd90a293e66f53efa7d40b48347ab1c2be07ea
Esteban Munoz <esteban.230@hotmail.com>
2019-12-21 00:12:35 -0700

    removing react-game-kit, will use different library

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,13 @@
 import React from 'react';
-import { AudioPlayer, Loop, Stage, KeyListener, World } from 'react-game-kit';
 import './App.css';
-import Matter from 'matter-js';
+import Game from "./Game/Game";
 
-export class App extends React.Component{
-  constructor(props: any){
-    super(props);
-    this.state = {
-      fade: true
-    };
-
-    this.keyListener = new KeyListener();
-    window.AudioContext = window.AudioContext
-  }
-
-  physicsInit(engine) {
-    const ground = Matter.Bodies.rectangle(512 * 3, 448, 1024 * 3, 64, {
-      isStatic: true,
-    });
-
-    const leftWall = Matter.Bodies.rectangle(-64, 288, 64, 576, {
-      isStatic: true,
-    });
-
-    const rightWall = Matter.Bodies.rectangle(3008, 288, 64, 576, {
-      isStatic: true,
-    });
-
-    Matter.World.addBody(engine.world, ground);
-    Matter.World.addBody(engine.world, leftWall);
-    Matter.World.addBody(engine.world, rightWall);
-  };
-
-  render(){
-    return (
-      <Loop>
-        <Stage style={{ background: '#3a9bdc' }}>
-          <World onInit={this.physicsInit}>
-          </World>
-        </Stage>
-      </Loop>
-    );
-  }
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Game/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Game/Game.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+class Game extends React.Component<any>{
+
+}
+
+export default Game;
+
