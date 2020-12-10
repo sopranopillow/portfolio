41680b594fd85fb468c0a8c088b04b8585f582e0
Esteban Munoz <esteban.230@hotmail.com>
2020-12-09 22:42:58 -0700

    initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import { Home } from './pages';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Home = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    )
+};
