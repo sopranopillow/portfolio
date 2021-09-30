a76f725bb1b91db6335a7100faefcd650af08ad6
Esteban Munoz <esteban@Estebans-MacBook-Pro.local>
2021-09-30 15:39:35 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7e7a1960a7e3f32dbf23e368779af131.js"
+  "/portfolio/precache-manifest.c377a4572b42a618dfe2b0df9a1defbf.js"
 );
 
 self.addEventListener('message', (event) => {
